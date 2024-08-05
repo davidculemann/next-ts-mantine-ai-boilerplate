f7c60e909433572bed307ecafee136283b710f27
davidculemann <davidculemann@gmail.com>
2024-08-05 22:15:11 +0100

    install mantine and add provider and enable tree shaking

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,5 @@
+import { ColorSchemeScript, createTheme, MantineProvider } from "@mantine/core";
+import "@mantine/core/styles.css";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
@@ -8,10 +10,19 @@ export const metadata = {
     description: "Examples of using the Vercel AI SDK with Next.js and OpenAI.",
 };
 
+const theme = createTheme({
+    /** Put your mantine theme override here */
+});
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <html lang="en">
-            <body className={inter.className}>{children}</body>
+            <head>
+                <ColorSchemeScript />
+            </head>
+            <body className={inter.className}>
+                <MantineProvider theme={theme}>{children} </MantineProvider>
+            </body>
         </html>
     );
 }
