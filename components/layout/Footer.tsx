d7bd2b88bf4775cb928c9b565c76826b8f043a82
davidculemann <davidculemann@gmail.com>
2024-08-10 11:29:01 +0100

    add shadcn

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -1,25 +1,3 @@
-import { ActionIcon, Container, Group, rem } from "@mantine/core";
-import { IconBrandInstagram, IconBrandTwitter, IconBrandYoutube } from "@tabler/icons-react";
-import Logo from "../shared/Logo";
-import classes from "../styles/FooterSocial.module.css";
-
 export function Footer() {
-    return (
-        <div className={classes.footer}>
-            <Container className={classes.inner}>
-                <Logo />
-                <Group gap={0} className={classes.links} justify="flex-end" wrap="nowrap">
-                    <ActionIcon size="lg" color="gray" variant="subtle">
-                        <IconBrandTwitter style={{ width: rem(18), height: rem(18) }} stroke={1.5} />
-                    </ActionIcon>
-                    <ActionIcon size="lg" color="gray" variant="subtle">
-                        <IconBrandYoutube style={{ width: rem(18), height: rem(18) }} stroke={1.5} />
-                    </ActionIcon>
-                    <ActionIcon size="lg" color="gray" variant="subtle">
-                        <IconBrandInstagram style={{ width: rem(18), height: rem(18) }} stroke={1.5} />
-                    </ActionIcon>
-                </Group>
-            </Container>
-        </div>
-    );
+    return <div>Footer</div>;
 }
