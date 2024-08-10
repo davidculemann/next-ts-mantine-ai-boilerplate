3f9fe26d563ddac3511ea11b1851631c2c3f4a30
davidculemann <davidculemann@gmail.com>
2024-08-10 19:16:04 +0100

    remove hidden class in mobile view

--- components/layout/app-sidebar.tsx
+++ components/layout/app-sidebar.tsx
@@ -4,7 +4,7 @@ import { Tooltip, TooltipContent, TooltipTrigger } from "../ui/tooltip";
 
 export function SideBar() {
     return (
-        <aside className="fixed inset-y-0 left-0 z-10 hidden w-14 flex-col border-r bg-background sm:flex">
+        <aside className="fixed inset-y-0 left-0 z-10 w-14 flex-col border-r bg-background sm:flex">
             <nav className="flex flex-col items-center gap-4 px-2 py-4">
                 <Link
                     href="#"
