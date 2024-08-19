51ebfecc1fbbcc253ad30b08d817b32365c614e3
davidculemann <davidculemann@gmail.com>
2024-08-19 11:53:10 +0900

    loading spinner while checking auth state

--- app/app/layout.tsx
+++ app/app/layout.tsx
@@ -3,20 +3,21 @@
 import { Footer } from "@/components/layout/footer";
 import Header from "@/components/layout/header";
 import { SideBar } from "@/components/layout/sidebar";
+import { LoadingSpinner } from "@/components/shared/loading-spinner";
 import { useAuthRedirect } from "@/lib/hooks/useAuthRedirect";
 import { useUser } from "@/lib/hooks/useUser";
 import Layout from "../root-layout";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     useAuthRedirect();
-    const [user, loading, error] = useUser();
+    const [user] = useUser();
 
-    if (loading) {
-        return <Layout>Loading...</Layout>;
-    }
-
-    if (error || !user) {
-        return <Layout>Access prohibited</Layout>;
+    if (!user) {
+        return (
+            <Layout>
+                <LoadingSpinner className="m-auto" size={60} />
+            </Layout>
+        );
     }
 
     return (
